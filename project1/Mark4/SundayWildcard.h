776ca25a31ca805f872c314bcc1b79cbaf7eec15
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-08 17:48:43 +0200

    Project 1 Mark 4 Sunday added and Mark 4 done

--- project1/Mark4/SundayWildcard.h
+++ project1/Mark4/SundayWildcard.h
@@ -1,22 +1,35 @@
-#ifndef BRUTEFORCE_WILDCARD_H
-#define BRUTEFORCE_WILDCARD_H
+#ifndef SUNDAY_WILDCARD_H
+#define SUNDAY_WILDCARD_H
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-bool bruteforce(string &pat, string &txt){
-    int p_len = pat.size(), t_len = txt.size();
+vector<int> preprocess(string &pat) {
+    int p_len = pat.length();
+
+    vector<int> shift(256, p_len + 1);
+
+    for (int i = 0; i < p_len; i++) {
+        shift[pat[i]] = p_len - i;
+    }
+
+    return shift;
+}
+
+bool sunday(string &pat, string &txt) {
+    int p_len = pat.length(), t_len = txt.length();
+
+    vector<int> shift = preprocess(pat);
 
     int i = 0;
-
-    for (int i = 0; i <= t_len - p_len; i++) {
+    while (i <= t_len - p_len) {
         int j = 0, k = 0;
         bool check = true;
 
-        while(j < p_len){
+        while (j < p_len) {
             if(pat[j] == '\\'){
                 check = false;
                 j++;
@@ -41,16 +54,22 @@ bool bruteforce(string &pat, string &txt){
                     continue;
                 }
             }
-            
             if (txt[i + j + k] != pat[j]) break;
             j++;
         }
-        
-        if(j == p_len){
+
+        if (j == p_len) {
             return true;
         }
+
+        if (i + p_len >= t_len) {
+            break;
+        }
+
+        i += shift[txt[i + p_len]];
     }
+
     return false;
 }
 
-#endif  // BRUTEFORCE_WILDCARD_H
+#endif  // SUNDAY_WILDCARD_H
--- project1/Mark4/main.cpp
+++ project1/Mark4/main.cpp
@@ -3,16 +3,23 @@
 #include <string>
 
 #include "BruteForceWildcard.h"
+#include "SundayWildcard.h"
 
 using namespace std;
 
 int main(){
     string text     = "abcdefbcabcdef";
-    string pattern  = "a*";
+    string pattern  = "defbca";
 
-    bool ans = bruteforce(pattern, text);
+    bool ans1 = bruteforce(pattern, text);
+    bool ans2 = sunday(pattern, text);
 
-    if(ans) cout << "True" << endl;
+    cout << "Brute Force: ";
+    if(ans1) cout << "True" << endl;
+    else cout << "False" << endl;
+
+    cout << "Sunday: ";
+    if(ans2) cout << "True" << endl;
     else cout << "False" << endl;
 
     return 0;
