998f7d5285c0452a5a2bd443a69af4bf7d493792
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-16 19:03:39 +0200

    Project 2 Mark 5 Time Measurement added

--- project2/Mark5/main.cpp
+++ project2/Mark5/main.cpp
@@ -1,45 +1,88 @@
+#include <algorithm>
+#include <chrono>
+#include <cassert>
+#include <random>
 #include <iostream>
 #include <vector>
-#include <list>
-#include <stdexcept>
-#include <cassert>
+#include <string>
 
+#include "./SeparateChainingHashTable.h"
+#include "./LinearProbingHashTable.h"
 #include "./DoubleHashingHashTable.h"
 
-using namespace std;
+using std::chrono::nanoseconds;
+
+constexpr int step = 10;
+constexpr int maxlen = 100;
+constexpr int times = 10;
 
 int main()
 {
-    DoubleHashingHashTable<string, int> hashTable(10);
+    std::cout << "Insert Time" << std::endl;
+    std::cout << "Seperate Chaining"
+              << " | "
+              << "Linear Probing"
+              << " | "
+              << "Double Hashing" << std::endl;
 
-    hashTable.insert("apple", 5);
-    hashTable.insert("banana", 7);
-    hashTable.insert("orange", 3);
+    nanoseconds scht_insert_time(0);
+    nanoseconds lpht_insert_time(0);
+    nanoseconds dhht_insert_time(0);
 
-    try
+    SeparateChainingHashTable<string, int> SCHashTable(10);
+    LinearProbingHashTable<string, int> LPHashTable(10);
+    DoubleHashingHashTable<string, int> DHHashTable(10);
+
+    for (int t = 0; t < times; t++)
     {
-        hashTable.insert("apple", 5);
-        assert(false);
-    }
-    catch (const invalid_argument &e)
-    {
-        assert(true);
+        auto begin = std::chrono::steady_clock::now();
+        SCHashTable.insert("key" + std::to_string(t), t);
+        auto end = std::chrono::steady_clock::now();
+        scht_insert_time += end - begin;
+
+        begin = std::chrono::steady_clock::now();
+        LPHashTable.insert("key" + std::to_string(t), t);
+        end = std::chrono::steady_clock::now();
+        lpht_insert_time += end - begin;
+
+        begin = std::chrono::steady_clock::now();
+        DHHashTable.insert("key" + std::to_string(t), t);
+        end = std::chrono::steady_clock::now();
+        dhht_insert_time += end - begin;
     }
 
-    assert(hashTable.search("apple") == 5);
-    assert(hashTable.search("banana") == 7);
+    std::cout << scht_insert_time.count() / times << " | " << lpht_insert_time.count() / times << " | " << dhht_insert_time.count() / times << std::endl;
 
-    hashTable.remove("banana");
+    std::cout << "Search Time" << std::endl;
+    std::cout << "Seperate Chaining"
+              << " | "
+              << "Linear Probing"
+              << " | "
+              << "Double Hashing" << std::endl;
 
-    try
+    nanoseconds scht_search_time(0);
+    nanoseconds lpht_search_time(0);
+    nanoseconds dhht_search_time(0);
+
+    for (int t = 0; t < times; t++)
     {
-        hashTable.search("banana");
-        assert(false);
+        auto begin = std::chrono::steady_clock::now();
+        SCHashTable.search("key" + std::to_string(t));
+        auto end = std::chrono::steady_clock::now();
+        scht_search_time += end - begin;
+
+        begin = std::chrono::steady_clock::now();
+        LPHashTable.search("key" + std::to_string(t));
+        end = std::chrono::steady_clock::now();
+        lpht_search_time += end - begin;
+
+        begin = std::chrono::steady_clock::now();
+        DHHashTable.search("key" + std::to_string(t));
+        end = std::chrono::steady_clock::now();
+        dhht_search_time += end - begin;
     }
-    catch (const out_of_range &e)
-    {
-        assert(true);
-    }
+
+    std::cout << scht_search_time.count() / times << " | " << lpht_search_time.count() / times << " | " << dhht_search_time.count() / times << std::endl;
 
     return 0;
 }
