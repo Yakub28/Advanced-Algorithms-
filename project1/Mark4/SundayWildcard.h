27c88ac8862ea322451312c7bae02b87f4964a66
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-24 17:53:50 +0200

    Sunday Algorithm with Wildcard Matching added

--- project1/Mark4/SundayWildcard.h
+++ project1/Mark4/SundayWildcard.h
@@ -7,66 +7,129 @@
 
 using namespace std;
 
-vector<int> preprocess(string &pat) {
+struct Processed
+{
+    vector<int> shift;
+    int q_shift, s_shift, w_count, e_count;
+};
+
+Processed preprocess(string &pat)
+{
+    int q_shift = -1, s_shift = -1, w_count = 0, e_count = 0;
     int p_len = pat.length();
-    // Initialize shift table with default shift value
+
     vector<int> shift(256, p_len + 1);
-    // Update shift values for each character in the pattern
-    for (int i = 0; i < p_len; i++) {
+
+    for (int i = 0; i < p_len; i++)
+    {
+        if (pat[i] == '?' && (i == 0 || pat[i - 1] != '\\'))
+        {
+            q_shift = p_len - i;
+            w_count++;
+        }
+        else if (pat[i] == '*' && (i == 0 || pat[i - 1] != '\\'))
+        {
+            s_shift = p_len - i;
+            w_count++;
+        }
+        else if (pat[i] == '\\' && (pat[i + 1] == '*' || pat[i + 1] == '?'))
+        {
+            i++;
+            e_count++;
+        }
         shift[pat[i]] = p_len - i;
     }
 
-    return shift;
+    Processed p = {shift, q_shift, s_shift, w_count, e_count};
+    return p;
 }
 
-bool sunday(string pat, string txt) {
-    int p_len = pat.length(), t_len = txt.length();
-    // Generate shift table using the function
-    vector<int> shift = preprocess(pat);
+bool sunday(string pat, string txt)
+{
+    Processed p = preprocess(pat);
+
+    int p_len = pat.length(),
+        t_len = txt.length(),
+        wildcard_count = p.w_count,
+        question_shift = p.q_shift,
+        star_shift = p.s_shift,
+        escape_count = p.e_count;
+
+    vector<int> shift = p.shift;
 
     int i = 0;
-    while (i <= t_len - p_len) {
+    while (i <= t_len - (p_len - escape_count))
+    {
         int j, k = 0;
 
-        for (j = 0; j < p_len; j++){
+        for (j = 0; j < p_len; j++)
+        {
             // Check for escape character '\'
-            if(pat[j] == '\\'){
+            if (pat[j] == '\\')
+            {
                 j++;
                 k++;
-                if (txt[i + j - k] != pat[j]) break;
+                if (txt[i + j - k] != pat[j])
+                    break;
                 continue;
             }
-            if (pat[j] == '?') {
+            if (pat[j] == '?')
+            {
                 continue;
-            } 
-            else if (pat[j] == '*') {
-                 // Skip consecutive '*' and '?' characters
-                while (pat[j+1] == '*' || pat[j+1] == '?') {
+            }
+            else if (pat[j] == '*')
+            {
+                if (j == p_len - 1)
+                    return true;
+                // Skip consecutive '*' and '?' characters
+                while (pat[j + 1] == '*' || pat[j + 1] == '?')
+                {
                     j++;
                 }
                 // Recursive call for remaining pattern and text
-                for (int y = 0; i + j + y < t_len; y++) {
-                    if (sunday((pat.substr(j+1)), (txt.substr(i + j + y - k)))) {
+                for (int y = 0; i + j + y < t_len; y++)
+                {
+                    if (sunday((pat.substr(j + 1)), (txt.substr(i + j + y - k))))
+                    {
                         return true;
                     }
                 }
                 return false;
-            } 
+            }
 
-            if (txt[i + j - k] != pat[j]) break;
+            if (txt[i + j - k] != pat[j])
+                break;
         }
 
-        if (j == p_len){
+        if (j == p_len)
+        {
             return true;
         }
-        if (i + p_len >= t_len) {
+        if (i + p_len >= t_len)
+        {
             break;
         }
 
-        i += shift[txt[i + p_len]];
+        char c = txt[i + p_len - escape_count];
+        if (wildcard_count == 0 || shift[c] < p_len + 1)
+        {
+            i += shift[c];
+        }
+        else if (star_shift == -1 || (question_shift != -1 && question_shift < star_shift))
+        {
+            i += question_shift;
+            // cout << txt[i] << endl;
+        }
+        else if (star_shift != -1)
+        {
+            if (sunday(pat.substr(0, p_len - star_shift), txt.substr(i + 1, i + star_shift + 1)))
+                if (sunday(pat.substr(p_len - star_shift), txt.substr(i + star_shift)))
+                    return true;
+            i += star_shift;
+        }
     }
 
     return false;
 }
 
-#endif  // SUNDAY_WILDCARD_H
+#endif // SUNDAY_WILDCARD_H
--- project1/Mark4/main.cpp
+++ project1/Mark4/main.cpp
@@ -1,41 +1,64 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
 
 #include "BruteForceWildcard.h"
 #include "SundayWildcard.h"
 
 using namespace std;
 
-int main(){
-    string text     = "he*lo world";
-    string pattern  = "he\\*lo";
+int main()
+{
+    string pattern0 = "he\\*lo", text0 = "he*lo world";
+    assert(sunday(pattern0, text0) == true);
+    assert(bruteforce(pattern0, text0) == true);
+    cout << "Passed 0" << endl;
 
-    bool ans1 = bruteforce(pattern, text);
-    bool ans2 = sunday(pattern, text);
+    string pattern1 = "he*lo", text1 = "helllllo world";
+    assert(sunday(pattern1, text1) == true);
+    assert(bruteforce(pattern1, text1) == true);
+    cout << "Passed 1" << endl;
 
-    cout << "Brute Force: ";
-    if(ans1) cout << "True" << endl;
-    else cout << "False" << endl;
+    string pattern2 = "he*llo", text2 = "hello world";
+    assert(sunday(pattern2, text2) == true);
+    assert(bruteforce(pattern2, text2) == true);
+    cout << "Passed 2" << endl;
 
-    cout << "Sunday: ";
-    if(ans2) cout << "True" << endl;
-    else cout << "False" << endl;
-    
-    string pattern4 = "he*lo", text4 = "helllllo world";
-    assert(bruteforce(pattern4, text4) == true);
+    string pattern3 = "e\\*?", text3 = "he*lloo world";
+    assert(sunday(pattern3, text3) == true);
+    assert(bruteforce(pattern3, text3) == true);
+    cout << "Passed 3" << endl;
+
+    string pattern4 = "e\\**", text4 = "he*lloo world";
     assert(sunday(pattern4, text4) == true);
+    assert(bruteforce(pattern4, text4) == true);
+    cout << "Passed 4" << endl;
 
+    string pattern5 = "e\\*\\*", text5 = "he*lloo world";
+    assert(sunday(pattern5, text5) == false);
+    assert(bruteforce(pattern5, text5) == false);
+    cout << "Passed 5" << endl;
 
-    string pattern6 = "he*llo", text6 = "hello world";
-    assert(bruteforce(pattern6, text6) == true);
+    string pattern6 = "e\\*?*", text6 = "he*lloo world";
     assert(sunday(pattern6, text6) == true);
+    assert(bruteforce(pattern6, text6) == true);
+    cout << "Passed 6" << endl;
 
+    string pattern7 = "e\\*", text7 = "he*lloo world";
+    assert(sunday(pattern7, text7) == true);
+    assert(bruteforce(pattern7, text7) == true);
+    cout << "Passed 7" << endl;
 
-    string pattern9 = "he\\*llo", text9 = "he*lloo world";
-    assert(bruteforce(pattern9, text9) == true);
+    string pattern8 = "e\\*?*w", text8 = "he*lloo world";
+    assert(sunday(pattern8, text8) == true);
+    assert(bruteforce(pattern8, text8) == true);
+    cout << "Passed 8" << endl;
+
+    string pattern9 = "e\\**w", text9 = "he*lloo world";
     assert(sunday(pattern9, text9) == true);
+    assert(bruteforce(pattern9, text9) == true);
+    cout << "Passed 9" << endl;
 
     return 0;
 }
